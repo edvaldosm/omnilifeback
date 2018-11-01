5936d16d23e7ecdb4db084efecf255ccd9971131
Edvaldo Santiago <edvaldo.santiago@gmail.com>
2018-11-01 01:02:39 -0300

    Correcao de Cross Domain nos endpoint´s

--- src/main/java/com/br/omnilife/restcontroller/ClienteRestController.java
+++ src/main/java/com/br/omnilife/restcontroller/ClienteRestController.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -27,6 +28,7 @@ import com.br.omnilife.service.ClienteService;
 import io.swagger.annotations.ApiOperation;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 @RequestMapping(value = "/cliente")
 public class ClienteRestController {
 
--- src/main/java/com/br/omnilife/restcontroller/ComprasRestController.java
+++ src/main/java/com/br/omnilife/restcontroller/ComprasRestController.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -26,6 +27,7 @@ import com.br.omnilife.service.ComprasService;
 import io.swagger.annotations.ApiOperation;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 @RequestMapping(value = "/compras")
 public class ComprasRestController {
 
